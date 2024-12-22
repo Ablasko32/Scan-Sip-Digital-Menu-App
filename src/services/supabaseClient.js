efec4d548dd5e42dfdacb0323377a0628a7e10b7
ante <an.blaskovic@gmail.com>
2024-12-22 11:15:52 +0100

    supabase client fix

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ksyctoiguomeuiavrbpf.supabase.c";
+const supabaseUrl = "https://ksyctoiguomeuiavrbpf.supabase.co";
 const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_API_KEY;
 
 const supabase = createClient(supabaseUrl, SUPABASE_KEY);
